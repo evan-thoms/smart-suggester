8e565bfdd438a24663f7f154c78fa04f1d8d7960
milan090 <milanmuhammed1@gmail.com>
2020-07-16 11:12:39 +0530

    Updates

---
+++ precache-manifest.8a2aa3c3bb42b95f8d1aec62053b50e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b1c85b405d04f05d9902d0c332ee65d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc52d458c623f99a2ed2",
+    "url": "/static/css/main.d4aba561.chunk.css"
+  },
+  {
+    "revision": "2778c44e08529acac20e",
+    "url": "/static/js/2.4c7724b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4c7724b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc52d458c623f99a2ed2",
+    "url": "/static/js/main.b150384c.chunk.js"
+  },
+  {
+    "revision": "4a1b9046ee1861f3f896",
+    "url": "/static/js/runtime-main.68a9f1ea.js"
+  }
+]);
